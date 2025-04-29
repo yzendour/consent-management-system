2052e00843c4bdf741e434b6a34c6458f9a3dbbe
Zendour Younes <y.zendour2@gmail.com>
2025-04-29 12:01:37 +0200

    -initial project setup

---
+++ src/main/java/com/consent/consentmanagementsystem/ConsentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.consent.consentmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsentManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsentManagementSystemApplication.class, args);
+    }
+
+}
